a42910b793ea9e94da904457c1cb9243e914d658
rabin <rabin@DESKTOP-3DBK7S2>
2020-03-25 23:08:00 +0545

    removed unused imports

--- src/main/java/com/rdongol/rentcollection/controller/UserController.java
+++ src/main/java/com/rdongol/rentcollection/controller/UserController.java
@@ -3,9 +3,6 @@ package com.rdongol.rentcollection.controller;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
