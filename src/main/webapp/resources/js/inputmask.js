186afea156d4d5dd701bb1e2aefa708b905b7dcc
rabin <rawbean08@gmail.com>
2020-05-18 15:28:40 +0545

    fixed bug with input mask

--- src/main/webapp/resources/js/inputmask.js
+++ src/main/webapp/resources/js/inputmask.js
@@ -1,22 +1,30 @@
 jQuery(function(e){
 	"use strict";
 	
-	jQuery(".phonenumber-inputmask").inputmask("9999999999"),
+	jQuery("form").on("click",".phonenumber-inputmask",function(){
+		
+		jQuery(this).inputmask("9999999999")
+	})
 	
-	jQuery(".email-inputmask").inputmask({
-        mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[*{2,6}][*{1,2}].*{1,}[.*{2,6}][.*{1,2}]",
-        greedy: !1,
-        onBeforePaste: function(n, a) {
-            return (e = e.toLowerCase()).replace("mailto:", "")
-        },
-        definitions: {
-            "*": {
-                validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~/-]",
-                cardinality: 1,
-                casing: "lower"
-            }
-        }
-    })
+	jQuery("form").on("click",".email-inputmask",function(){
+		
+		jQuery(this).inputmask({
+			mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[*{2,6}][*{1,2}].*{1,}[.*{2,6}][.*{1,2}]",
+			greedy: !1,
+			onBeforePaste: function(n, a) {
+				return (e = e.toLowerCase()).replace("mailto:", "")
+			},
+			definitions: {
+				"*": {
+					validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~/-]",
+					cardinality: 1,
+					casing: "lower"
+				}
+			}
+		})
+		
+	})
+
 	
 	
 })
